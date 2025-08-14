eca8aa8d52f59bfcad13aa512441ffe9f401fc7c
petertechy <petertechy01@gmail.com>
2025-08-14 12:30:00 +0100

    Update routes/user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { addUser, editUser } = require("../controllers/userController")
+const { addUser, editUser, authenticateUser } = require("../controllers/userController")
 
 const router = express.Router()
 
 
 router.post("/register", addUser)
 router.post("/edit", editUser)
+router.post("/signin", authenticateUser)
 
 module.exports = router;
