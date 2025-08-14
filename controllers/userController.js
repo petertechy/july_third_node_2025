4622e546b2c6947ae680db8118305ec376ef6a45
petertechy <petertechy01@gmail.com>
2025-08-14 12:00:00 +0100

    Update controllers/userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const UserModel = require("../models/userModel");
+
+const addUser = (req, res) => {
+  let form = new UserModel(req.body);
+  form.save().then(() => {
+    console.log("User info Saved");
+    // console.log(form);
+    res.send({ status: true, message: "Correct Submission", form });
+  });
+};
+
+const authenticateUser = (req, res) =>{
+  // console.log(req.body)
+  let {password} = req.body
+  UserModel.findOne({email: req.body.email})
+  .then((response)=>{
+    if(response){
+      //email is valid
+      response.validatePassword(password,(err, same)=>{
+        if(!same){
+          res.send({status: false, message: "wrong details"})
+        }else{
+          res.send({status: true, message: "user logged in"})
+
+        }
+      })
+    }else{
+      console.log("Wrong Credentials")
+      res.send({status: false, message: "wrong credentials"})
+    }
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+}
+
+const editUser = () => {};
+
+module.exports = { addUser, editUser, authenticateUser };
