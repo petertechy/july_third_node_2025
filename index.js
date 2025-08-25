d504ecf34557b104f001e1f321a5a4e07b3c7af5
Petertechy <petertechy01@gmail.com>
2025-08-25 22:45:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
-const PORT = 5000
 const userRoute = require("./routes/user.route")
+require('dotenv').config()
+const PORT = process.env.PORT
 // app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(express.json());
 app.use(cors())
 app.use("/", userRoute)
 
-const URI = "mongodb+srv://petertechy01:Olanrewaju@my-project.w3cyijl.mongodb.net/student_db?retryWrites=true&w=majority&appName=my-project"
+const URI = process.env.MONGODB_URI
 mongoose.connect(URI)
 .then(()=>{
     console.log("Mongodb iyaf connected successfully");
