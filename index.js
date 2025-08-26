16340bc568d45580f63913b35dcf1a56db5f9e1f
Petertechy <petertechy01@gmail.com>
2025-08-26 20:06:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require("cors")
 const userRoute = require("./routes/user.route")
 require('dotenv').config()
 const PORT = process.env.PORT
-// app.use(express.urlencoded({ extended: true, limit: "30mb" }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(express.json({limit: "30mb"}));
 app.use(cors())
 app.use("/", userRoute)
 
